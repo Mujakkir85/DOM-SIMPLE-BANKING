edadbac954127aca79eeabdc618253ed739773d1
Mujakkir85 <mujakkirlucse@gmail.com>
2022-02-16 17:56:40 +0600

    total balance

--- app.js
+++ app.js
@@ -13,6 +13,23 @@ function getDepositOrWithdraw(totalID, getDepositInputOrWithdrawInput) {
     setTotalDepositOrWithdraw.innerText = addTotalDepositOrWithdraw;
 }
 
+// Total balance
+
+function showTotalBalance(totalDepositOrWithdraw, isTrue) {
+
+    let TotalBalanceField = document.getElementById('balance-total');
+    const getTotalBalance = parseFloat(TotalBalanceField.innerText);
+
+    if (isTrue) {
+        const newTotalBlance = getTotalBalance + totalDepositOrWithdraw;
+        TotalBalanceField.innerText = newTotalBlance;
+    }
+    else {
+        const newTotalBlance = getTotalBalance - totalDepositOrWithdraw;
+        TotalBalanceField.innerText = newTotalBlance;
+    }
+
+}
 
 //deposit 
 
@@ -20,6 +37,7 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     let getDepositInput = getInputValue('deposit-input');
     //console.log('dep', getDepositInput)
     getDepositOrWithdraw('deposit-total', getDepositInput);
+    showTotalBalance(getDepositInput, true)
 })
 
 //withdraw
@@ -27,4 +45,5 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
     let getWithdrawInput = getInputValue('withdraw-input')
     // console.log('wit', getWithdrawInput)
     getDepositOrWithdraw('withdraw-total', getWithdrawInput);
+    showTotalBalance(getWithdrawInput, false)
 })
